76870afd0a5864853b4e99ce1b8190cfcca2cdb8
Diego Alvarez <diego.canada@icloud.com>
2022-12-06 10:46:48 -0800

    format

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -7,19 +7,14 @@ pub fn part_one(input: &str) -> Option<u32> {
         .filter(|l| !l.is_empty())
         .tuples::<(&str, &str)>()
         .map(|t| match t {
-            // X for Rock, Y for Paper, and Z for Scissors.
-            // A for Rock, B for Paper, and C for Scissors.
-
             // draw
             ("A", "X") => 1 + 3,
             ("B", "Y") => 2 + 3,
             ("C", "Z") => 3 + 3,
-
             // win
             ("A", "Y") => 2 + 6,
             ("B", "Z") => 3 + 6,
             ("C", "X") => 1 + 6,
-
             // lose
             (_, "X") => 1,
             (_, "Y") => 2,
@@ -32,35 +27,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    // X for Rock, Y for Paper, and Z for Scissors.
+    // A for Rock, B for Paper, and C for Scissors.
+
+    // X means you need to lose,
+    // Y means you need to end the round in a draw, and
+    // Z means you need to win. Good luck!"
+
     let x = input
         .split("\n")
         .flat_map(|l| l.split(" "))
         .filter(|l| !l.is_empty())
         .tuples::<(&str, &str)>()
         .map(|t| match t {
-            // X for Rock, Y for Paper, and Z for Scissors.
-            // A for Rock, B for Paper, and C for Scissors.
-
-            // X means you need to lose,
-            // Y means you need to end the round in a draw, and
-            // Z means you need to win. Good luck!"
-
             // lose X
             ("A", "X") => 3,
             ("B", "X") => 1,
             ("C", "X") => 2,
-
             // draw Y
             ("A", "Y") => 1 + 3,
             ("B", "Y") => 2 + 3,
             ("C", "Y") => 3 + 3,
-
             // win Z
             ("A", "Z") => 2 + 6,
             ("B", "Z") => 3 + 6,
             ("C", "Z") => 1 + 6,
-
-
             (&_, _) => 0,
         })
         .sum::<u32>();
