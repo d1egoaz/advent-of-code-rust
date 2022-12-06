0456a97377def6f9069aec647927bc5139037c95
Diego Alvarez <diego.canada@icloud.com>
2022-12-06 10:30:27 -0800

    day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -20,10 +20,11 @@ pub fn part_one(input: &str) -> Option<u32> {
             ("B", "Z") => 3 + 6,
             ("C", "X") => 1 + 6,
 
+            // lose
             (_, "X") => 1,
             (_, "Y") => 2,
             (_, "Z") => 3,
-            (&_, _) => 10_000,
+            (&_, _) => 0,
         })
         .sum::<u32>();
 
@@ -31,7 +32,40 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let x = input
+        .split("\n")
+        .flat_map(|l| l.split(" "))
+        .filter(|l| !l.is_empty())
+        .tuples::<(&str, &str)>()
+        .map(|t| match t {
+            // X for Rock, Y for Paper, and Z for Scissors.
+            // A for Rock, B for Paper, and C for Scissors.
+
+            // X means you need to lose,
+            // Y means you need to end the round in a draw, and
+            // Z means you need to win. Good luck!"
+
+            // lose X
+            ("A", "X") => 3,
+            ("B", "X") => 1,
+            ("C", "X") => 2,
+
+            // draw Y
+            ("A", "Y") => 1 + 3,
+            ("B", "Y") => 2 + 3,
+            ("C", "Y") => 3 + 3,
+
+            // win Z
+            ("A", "Z") => 2 + 6,
+            ("B", "Z") => 3 + 6,
+            ("C", "Z") => 1 + 6,
+
+
+            (&_, _) => 0,
+        })
+        .sum::<u32>();
+
+    Some(x)
 }
 
 fn main() {
@@ -53,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
