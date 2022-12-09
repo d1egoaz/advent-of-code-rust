330651dc5a120d52fa3d61bf4cd76816248ba3b0
Diego Alvarez <diego.canada@icloud.com>
2022-12-09 12:32:42 -0800

    day 6 part 1 and 2

---
+++ src/bin/06.rs
@@ -0,0 +1,53 @@
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<usize> {
+    find_position(input, 4)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    find_position(input, 14)
+}
+
+pub fn find_position(input: &str, total: usize) -> Option<usize> {
+    let chars: Vec<char> = input.chars().collect();
+    for (i, w) in chars.windows(total).enumerate() {
+        if w.iter().all_unique() {
+            return Some(i + total);
+        }
+    }
+
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(7));
+        assert_eq!(part_one("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(7));
+        assert_eq!(part_one("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+        assert_eq!(part_one("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+        assert_eq!(part_one("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
+        assert_eq!(part_one("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), Some(19));
+        assert_eq!(part_two("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(19));
+        assert_eq!(part_two("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(23));
+        assert_eq!(part_two("nppdvjthqldpwncqszvftbrmjlhg"), Some(23));
+        assert_eq!(part_two("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(29));
+        assert_eq!(part_two("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(26));
+    }
+}
